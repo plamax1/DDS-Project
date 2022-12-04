d28987e0c73e42c0a2a168f206c325cef36aa425
GianmarcoDIF <difelicegianmarco@gmail.com>
2022-12-04 17:58:10 +0100

    rabbitMQ

---
+++ rabbitmq/sender/src/main/java/rb/gianmarcodif/sender/Sender.java
@@ -0,0 +1,31 @@
+package rb.gianmarcodif.sender;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Component
+@RestController
+public class Sender implements CommandLineRunner {
+
+  private final RabbitTemplate rabbitTemplate;
+
+  public Sender(RabbitTemplate rabbitTemplate) {
+    this.rabbitTemplate = rabbitTemplate;
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    System.out.println("Sending message...");
+  }
+  @GetMapping("/{mesage}")
+  public String sendMessage(@PathVariable String message) {
+    rabbitTemplate.convertAndSend(SenderApplication.topicExchangeName, "foo.bar.baz", message);
+    return "Message sent: " + message;
+  }
+
+}
