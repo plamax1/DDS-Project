bc61ad0be3a460b9857e3dee880f8e620283a8f4
GianmarcoDIF <difelicegianmarco@gmail.com>
2022-12-05 00:56:22 +0100

    .

---
+++ rabbitq/receiver/src/test/java/rmq/gianmarcodif/receiver/ReceiverApplicationTests.java
@@ -0,0 +1,13 @@
+package rmq.gianmarcodif.receiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ rabbitq/sender/src/test/java/rmq/gianmarcodif/sender/SenderApplicationTests.java
@@ -0,0 +1,13 @@
+package rmq.gianmarcodif.sender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
