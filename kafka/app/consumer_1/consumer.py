52c3f1b95174c372761e87ecc72678a867108a9e
plamax1 <pietro.lamanna98@gmail.com>
2022-12-04 22:42:07 +0100

    consumer_1 starts from last msg, added rand group id

--- kafka/app/consumer_1/consumer.py
+++ kafka/app/consumer_1/consumer.py
@@ -1,17 +1,28 @@
 from kafka import KafkaConsumer
 from json import loads
 from time import sleep, time
+from random import randint
 import numpy as np
+
 latencies = []
+groupid=str(randint(100000000,999999999))
 consumer = KafkaConsumer(
     'topic_test',
     bootstrap_servers=['localhost:9092'],
     auto_offset_reset='earliest',
     enable_auto_commit=True,
-    group_id='group_1',
+    group_id=groupid,
     value_deserializer=lambda x: loads(x.decode('utf-8'))
 )
+print("Consumer id: " , groupid)
 event_counter=0;
+
+#dummy poll 
+consumer.poll()
+#go to end of the stream
+consumer.seek_to_end()
+
+# Event loop
 for event in consumer:
     event_counter+=1
     event_data = event.value
--- kafka/app/producer_live/producer.py
+++ kafka/app/producer_live/producer.py
@@ -1,12 +1,11 @@
 from time import sleep, time
 from json import dumps
 from kafka import KafkaProducer
-f = open("img.jpg", "r")
 producer = KafkaProducer(
     bootstrap_servers=['localhost:9092'],
     value_serializer=lambda x: dumps(x).encode('utf-8')
 )
-for j in range(101):
+for j in range(100):
     print("Iteration", j)
     data = {'counter-live': j}
     producer.send('topic_test', value=data, timestamp_ms = round(time()*1000))
