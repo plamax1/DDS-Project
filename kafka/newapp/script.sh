7332a7192d7c9654da3b2be259ca306bd869dcc8
plamax1 <pietro.lamanna98@gmail.com>
2022-12-06 04:03:19 +0100

    update bash script topic -no cleaning

--- kafka/newapp/script.sh
+++ kafka/newapp/script.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 #build the consumer container
 docker build ./consumer -t consumer
-TOPIC1="topic1"
-TOPIC2="topic2"
+TOPIC1="topic_test"
+TOPIC2="topic_test"
 for i in {1..5}
 do 
     docker run -d --network host  consumer $TOPIC1 
