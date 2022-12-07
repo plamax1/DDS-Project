98911a28d3cfbd3eacd4d22f395c21007d713846
plamax1 <pietro.lamanna98@gmail.com>
2022-12-07 10:17:38 +0100

    fix dockercompose, producer in newapp

--- kafka/newapp/producer_script.sh
+++ kafka/newapp/producer_script.sh
@@ -5,7 +5,7 @@ TOPIC1="topic_test"
 TOPIC2="topic_test"
 for i in {1..2}
 do 
-    docker run -d --network host  producer $TOPIC1 
+    docker run -d --network host  producer $TOPIC1 27 27
     echo "Producer number $i running and listening on topic $TOPIC1"
 done
 
