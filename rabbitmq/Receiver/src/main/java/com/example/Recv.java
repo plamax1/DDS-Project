c3e0dc4d74d9b442b1aa1176f535b0ba9001283d
GianmarcoDIF <difelicegianmarco@gmail.com>
2022-12-16 00:22:11 +0100

    rabbitmq: delay precision fixed. Message/second print.

--- rabbitmq/Receiver/src/main/java/com/example/Recv.java
+++ rabbitmq/Receiver/src/main/java/com/example/Recv.java
@@ -11,6 +11,16 @@ public class Recv {
     private final static String endMessage = "end";
     private int msgNumber;
     private int delaySum;
+    private long startTransmissionTime;
+
+    public long getStartTransmissionTime() {
+        return startTransmissionTime;
+    }
+
+    public void setStartTransmissionTime(long startTransmissionTime) {
+        this.startTransmissionTime = startTransmissionTime;
+    }
+
     public int getDelaySum() {
         return delaySum;
     }
@@ -30,9 +40,9 @@ public class Recv {
     public void setMsgNumber(int msgNumber) {
         this.msgNumber = msgNumber;
     }
-    
+
     public void incMsgNumber() {
-        this.msgNumber = msgNumber+1;
+        this.msgNumber = msgNumber + 1;
     }
 
     public static void main(String[] argv) throws Exception {
@@ -41,15 +51,14 @@ public class Recv {
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
 
-        //connect to the exchanger and get the queue name
+        // connect to the exchanger and get the queue name
         channel.exchangeDeclare(EXCHANGE_NAME, "topic");
         String queueName = channel.queueDeclare().getQueue();
 
-        
-        //get the environmant variable topic
+        // get the environmant variable topic
         String bindingKey = System.getenv("topic");
         channel.queueBind(queueName, EXCHANGE_NAME, bindingKey);
-        
+
         channel.queueBind(queueName, EXCHANGE_NAME, bindingKey);
 
         Recv r = new Recv();
@@ -57,26 +66,32 @@ public class Recv {
         r.setDelaySum(0);
         System.out.println("[*] Waiting for messages. Topic is " + bindingKey + ". To exit press CTRL+C.");
 
-
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
             String message = new String(delivery.getBody(), "UTF-8");
-            
-            if(!message.equals(endMessage)){
-            long rcvInstant= (long)delivery.getProperties().getHeaders().get("timestamp") ;
-            long delay = System.currentTimeMillis() - rcvInstant;
-            System.out.println(r.getMsgNumber() + "] Delay:["  + delay + " ms]Received '" +            
-            delivery.getEnvelope().getRoutingKey() + "':'" + message + "'");
-            r.incMsgNumber();
-            r.incDelaySum((int) delay);
-            }
-            else{
-                float avgDelay=r.getDelaySum() / r.getMsgNumber();
-                System.out.println(endMessage + " RECEIVED! The average delay is " + avgDelay);
+
+            if (!message.equals(endMessage)) {
+                long rcvInstant = (long) delivery.getProperties().getHeaders().get("timestamp");
+                long delay = System.currentTimeMillis() - rcvInstant;
+                System.out.println(r.getMsgNumber() + "] Delay:[" + delay + " ms]Received '" +
+                        delivery.getEnvelope().getRoutingKey() + "':'" + message + "'");
+                r.incMsgNumber();
+                r.incDelaySum((int) delay);
+            } else {
+                float avgDelay = (float)(((float)r.getDelaySum()) / ((float)r.getMsgNumber()));
+                long endTime = System.currentTimeMillis();
+                System.out.println(endMessage + " RECEIVED! The average delay is " + avgDelay + "ms.");
+                long difference = endTime - r.getStartTransmissionTime();
+                float throughput = (float)( (float)r.getMsgNumber() / (float)((float)1000 * (difference)));
+                System.out.println("You received " + r.getMsgNumber() + " messages in " + difference + "ms. So you received " + Math.pow(throughput, -1) +" messages per second.");
                 r.setDelaySum(0);
                 r.setMsgNumber(0);
             }
+            if (r.getMsgNumber() == 1) {
+                r.setStartTransmissionTime(System.currentTimeMillis());
+            }
         };
 
-        channel.basicConsume(queueName, true, deliverCallback, consumerTag -> { });
+        channel.basicConsume(queueName, true, deliverCallback, consumerTag -> {
+        });
     }
 }
