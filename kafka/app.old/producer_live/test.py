a24df3f51b1318c81477f295173449d1ab80c6c7
plamax1 <pietro.lamanna98@gmail.com>
2023-02-24 18:10:35 +0100

    deleted app.old

--- kafka/app.old/producer_live/test.py
+++
@@ -1,9 +0,0 @@
-import base64
-f = open("img.jpg", "r")
-a = open("img2.jpg", "w")
-base64_images = base64.b64encode(f)
-#message_string = f.encode('ascii')
-#new_image = message_string.decode('ascii')
-a.write(new_image)
-a.close()
-
