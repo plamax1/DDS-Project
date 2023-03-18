aff17c340b97e5258e6e9456db710ea7079899c3
GianmarcoDIF <difelicegianmarco@gmail.com>
2023-03-18 11:47:08 +0100

    Rabbitmq script: can create multiple producer in one shot

--- rabbitmq/producer_script.sh
+++ rabbitmq/producer_script.sh
@@ -7,7 +7,6 @@ i=0;
 t=0;
 while [ $i -le 0 ];
 do
-    ((t=t+1))
     read -p "Define a topic: " topic
     export topic=$topic
     read -p "How many messages should it send about $topic? : " messagesToSend
@@ -17,9 +16,13 @@ do
     read -p "OTHERWISE insert the message rate (msg/s) :" rate
     export rate=$rate
 
-    
-    docker run -d --network host -e topic -e messagesToSend -e poisson -e rate producer:1.0
-    echo "Producer number $t running and producing $messagesToSend on topic $topic"
+    read -p "How many producer do you want to create for topic $topic? : " createNumber
+    for j in `seq 1 $createNumber`;
+    do
+        ((t=t+1+i))
+        docker run -d --network host -e topic -e messagesToSend -e poisson -e rate producer:1.0
+        echo "Producer number $t running and producing $messagesToSend on topic $topic"
+    done
     
     read -p "Need more producer to generate? (Y/N): " go
     if [ $go == "N" ];
