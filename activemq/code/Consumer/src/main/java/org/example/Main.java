9bc78282b42a060f177ba4553a36836d24867e58
GianmarcoDIF <difelicegianmarco@gmail.com>
2023-03-22 23:56:38 +0100

    renamed activemq folders

--- activemq/code/Consumer/src/main/java/org/example/Main.java
+++ activemq/code/Consumer/src/main/java/org/example/Main.java
@@ -1,10 +1,6 @@
 package org.example;
 
 import org.example.messaging.MConsumer;
-import org.example.models.Message;
-
-import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,11 +8,6 @@ public class Main {
         String topic = System.getenv("TOPIC1");
         System.out.println("Name of the topic: " + topic);
         MConsumer.initialize("tcp://localhost:61616", topic);
-
-        try {
-            c = System.in.read();
-        } catch (Exception ex) {
-        }
 
         // MConsumer.terminate();
     }
